1ab4577bcde78ed95b8181f3938d0be376478f6e
unknown <daisysauspurchase@icloud.com>
2020-05-02 18:20:18 +0800

    Basically did nothing but wrote a very bad Serbia Stronk knock-off poem that doesn't even rhyme

---
+++ main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 17:50:42 2020
+
+
+From New York to Moskow
+to Moskow
+
+The entire pilipili global streamers are attacked
+streamers attacked
+
+Overieda lead the pilipili resistance
+lead pilipili resistance
+
+Everyone must see they are not afraid of anything
+they are not afraid of anything
+
+
+"""
+
+import requests  
+import os  
+import time  
+import json  
+import sys  
+import subprocess  
+import datetime  
+import getopt  
+import streamlink
+import streamlink_cli
+
+
+# The proxies used for accessing YouTube
+proxies = {
+  'http': 'http:127.0.0.1:7890',
+  'https': 'http:127.0.0.1:7890s',
+}
+
